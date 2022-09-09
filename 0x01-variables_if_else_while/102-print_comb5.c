cd2ce3fa616f866eb6e1d09d33c15203e633219c
James Kariuki <karisjaymo99@gmail.com>
2022-09-10 01:39:23 +0300

    use modulo in file 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,28 +14,14 @@ int main(void)
 		{
 			if (a < b)
 			{
-				if (a < 10)
-				{
-					putchar('0');
-					putchar(a + '0');
-				}
-				else
-				{
-					putchar(((a / 10) % 10) + '0');
-					putchar((a % 10) + '0');
-				}
+				putchar(((a / 10) % 10) + '0');
+				putchar((a % 10) + '0');
+
 				putchar(' ');
 
-				if (b < 10)
-				{
-					putchar('0');
-					putchar(b + '0');
-				}
-				else
-				{
-					putchar(((b / 10) % 10) + '0');
-					putchar((b % 10) + '0');
-				}
+				putchar(((b / 10) % 10) + '0');
+				putchar((b % 10) + '0');
+
 				if ((a == 98) && (b == 99))
 					continue;
 				else
