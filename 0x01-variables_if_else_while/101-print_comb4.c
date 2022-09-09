d4bb59ccc99bafa02352013a001c1b4bb09d7707
James Kariuki <karisjaymo99@gmail.com>
2022-09-10 00:51:02 +0300

    added file 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the c program
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				if ((a < b) && (b < c))
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+
+					if ((a == 7) && (b == 8) && (c == 9))
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+}
