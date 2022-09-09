c24f99aae792e110729c21512c7988fa0e43e232
James Kariuki <karisjaymo99@gmail.com>
2022-09-09 20:20:50 +0300

    add betty function documentation for main

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main entry for the program
+ */
 int main(void)
 {
 	int n;
