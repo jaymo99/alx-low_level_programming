b60663b39dd88ce9414e7908a9f85386c5aa52df
James Kariuki <karisjaymo99@gmail.com>
2022-09-14 15:59:53 +0300

    removed spaces for betty compliance

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -67,32 +67,32 @@ void print_numbers_ascending(int starting, int stopping)
      * int m: used for output in place of n
      * Very helpful when dealing with negative numbers
     */
-    int n = starting;
-    int m;
+	int n = starting;
+	int m;
 
-    for (n = n; n <= stopping; n++)
-    {
-    /* negative numbers */
-        if (n < 0)
-        {
-            m = n * -1;
-            _putchar('-');
-            print_all_digits(m);
-        }
-        else
-        {
-            print_all_digits(n);
-        }
+	for (n = n; n <= stopping; n++)
+	{
+		/* negative numbers */
+		if (n < 0)
+		{
+			m = n * -1;
+			_putchar('-');
+			print_all_digits(m);
+		}
+		else
+		{
+			print_all_digits(n);
+		}
 
-        /* add comma and space if the list is yet to reach 97 */
-        if (n < stopping)
-        {
-            _putchar(',');
-            _putchar(' ');
-        }
-        else
-            _putchar('\n');
-    }
+		/* add comma and space if the list is yet to reach 97 */
+		if (n < stopping)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+			_putchar('\n');
+	}
 
 }
 
