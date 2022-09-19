cc18636dbe8957233f5880cb60e135d8e72304b7
James Kariuki <karisjaymo99@gmail.com>
2022-09-19 21:54:23 +0300

    Task 7 - print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: the input string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i, n;
+
+	if ((len) % 2 != 0)
+	{
+		n = (len - 1) / 2;
+	}
+	else
+		n = len / 2;
+
+	for (i = n; i <= len; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
