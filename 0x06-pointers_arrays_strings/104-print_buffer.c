70d06a89d0cd042d8186a01cf7d632480158466f
James Kariuki <karisjaymo99@gmail.com>
2022-09-22 20:53:46 +0300

    pending optional tasks

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer to the buffer
+ * @size: number of bytes
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+
+}
