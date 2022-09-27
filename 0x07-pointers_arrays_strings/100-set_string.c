cc2f08543069757c52d6f0f990c5728eb7fabf48
James Kariuki <karisjaymo99@gmail.com>
2022-09-27 10:34:54 +0300

    Betty compliance for task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,8 +2,8 @@
 
 /**
  * set_string - sets the value of a pointer to a char.
- * @s: - 
- * @to: -
+ * @s: double pointer (pointer to pointer of type char)
+ * @to: pointer to char
  */
 void set_string(char **s, char *to)
 {
