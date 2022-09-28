b5cf982fdfdbefe801b848992105f5c5044470b4
James Kariuki <karisjaymo99@gmail.com>
2022-09-28 15:47:14 +0300

    Task 1 - print in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * display_string - prints all characters in a string (in reverse)
+ * @s: string to print
+ * @len: length of the string
+ */
+void display_string(char *s, int len)
+{
+	if (len == 0)
+		return;
+	_putchar(*(s + (len - 1)));
+	len--;
+
+	display_string(s, len);
+}
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	int len = strlen(s);
+	display_string(s, len);
+
+	_putchar('\n');
+}
