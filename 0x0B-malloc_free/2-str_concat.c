5633effa21b2ea94da24c99ff4c5dc98cef8e603
James Kariuki <karisjaymo99@gmail.com>
2022-10-05 08:18:18 +0300

    Handle malloc return, task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,18 @@
 char *str_concat(char *s1, char *s2)
 {
 
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-	int new_len = len1 + len2 + 1;
+	int len1;
+	int len2;
+	int new_len;
 	int i = 0, j = 0, counter = 0;
 	char *ptr = malloc(sizeof(char) * new_len);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	new_len = len1 + len2 + 1;
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
