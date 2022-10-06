08a5260afd4e8635eac172c7cb7aebde61d6c735
James Kariuki <karisjaymo99@gmail.com>
2022-10-06 21:58:41 +0300

    Task 2 - allocate memory for an array using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of array elements
+ * @size: size of each element
+ *
+ * Return: a pointer to the allocated memory.
+ * NULL, if @nmemb or @size is 0.
+ * NULL, if malloc fails.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
+}
