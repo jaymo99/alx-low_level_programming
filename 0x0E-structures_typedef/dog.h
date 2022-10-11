45280efd815a84d04b89312a0ffd2a4e1cb8ae6c
James Kariuki <karisjaymo99@gmail.com>
2022-10-11 22:21:19 +0300

    Task 4 - function creates a new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,9 @@ struct dog
 
 typedef struct dog dog_t;
 
-/* function prototype */
+/* function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif /* DOG_H */
