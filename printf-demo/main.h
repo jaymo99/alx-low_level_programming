cd51d1f729630465ed347d9c036953de49f5372f
James Kariuki <karisjaymo99@gmail.com>
2022-10-19 17:06:58 +0300

    add header file

--- printf-demo/main.h
+++ printf-demo/main.h
@@ -3,4 +3,11 @@
 
 int  _printf(const char *format, ...);
 int _putchar(char c);
+
+typedef struct Specifiers
+{
+	char symbol;
+	int (*f)();
+} Specifiers;
+
 #endif /* MAIN_H */
--- printf-demo/utils.c
+++ printf-demo/utils.c
@@ -1,1 +1,12 @@
-#include
+#include "main.h"
+
+/**
+ * format_specifier - identifies the indicated format
+ * @format: the original string
+ * @index: the index position of the '%' symbol that specifies
+ * format.
+ */
+void format_specifier(const char *format, int index)
+{
+
+}
