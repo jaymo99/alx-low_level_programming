6e97a493a0b25ea63f700547f78b5cf5faa54ac2
James Kariuki <karisjaymo99@gmail.com>
2022-10-25 09:19:54 +0300

    betty compliance & check for NULL arguments

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,15 @@ size_t print_list(const list_t *h)
 {
 	int counter = 1;
 
-	if(h->str == NULL)
+	if (h == NULL)
+		exit(1);
+
+	if (h->str == NULL)
 		printf("[%d] (nil)\n", h->len);
 	else
 		printf("[%d] %s\n", h->len, h->str);
-	
-	if(h->next != NULL)
+
+	if (h->next != NULL)
 		counter += print_list(h->next);
 
 	return (counter);
