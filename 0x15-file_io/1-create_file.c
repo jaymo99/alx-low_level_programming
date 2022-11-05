d79f2571499a922882b04f20aefd6360edfb574b
James Kariuki <karisjaymo99@gmail.com>
2022-11-05 10:22:05 +0300

    Truncate if file exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR, 00600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
@@ -35,5 +35,6 @@ int create_file(const char *filename, char *text_content)
 	if (count < 0)
 		return (-1);
 
+	close(fd);
 	return (1);
 }
