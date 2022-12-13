7d420c0e05432cdbf20a6b097d04768252b4919c
James Kariuki <karisjaymo99@gmail.com>
2022-12-13 18:12:23 +0300

    borrowed 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/jaymo99/alx-low_level_programming/blob/7af67ae6ea248e06efa5426b5cf69a11f76f9bf4/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libruin.so
+wget -P /tmp https://github.com/baccrie/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
