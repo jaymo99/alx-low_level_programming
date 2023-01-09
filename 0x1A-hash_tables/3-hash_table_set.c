e04315ccba9c4525370eecff386d5a17924bf2b0
James Kariuki <karisjaymo99@gmail.com>
2023-01-09 05:59:25 +0300

    betty compliance

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,6 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	/* Create new hash_node and populate values */
 	hash_node_t *node = malloc(sizeof(hash_node_t));
+
 	if (node == NULL)
 	{
 		return (0);
