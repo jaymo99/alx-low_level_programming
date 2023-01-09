e19ddb8181d251199950a88effceed763b488b59
James Kariuki <karisjaymo99@gmail.com>
2023-01-09 06:29:35 +0300

    Task 3 - add an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 #include "hash_tables.h"
 
@@ -16,17 +17,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	/* Create new hash_node and populate values */
-	hash_node_t *node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node = NULL;
+	hash_node_t *collision_node = NULL;
+	unsigned long int k_index;
+	size_t buffer_size = 0;
 
-	if (node == NULL)
+	/* Create new hash_node and populate values */
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
 	{
 		return (0);
 	}
 
+	buffer_size = strlen(key) + 1;
+	new_node->key = malloc(sizeof(char) * buffer_size);
+	if (new_node->key == NULL)
+	{
+		return (0);
+	}
+
+	strcpy(new_node->key, key);
+	strcpy(new_node->value, value);
+	new_node->next = NULL;
+
 	/* Get index of the key */
+	k_index = key_index((unsigned char *)new_node->key, ht->size);
 
-	/* Check for collision */
+	/* Insert key at the given index (check for collision) */
+	if (ht->array[k_index] == NULL)
+	{
+		ht->array[k_index] = new_node;
+	}
+	else
+	{
+		collision_node = ht->array[k_index];
+		new_node->next = collision_node;
+		ht->array[k_index] = new_node;
+	}
 
-	/* Insert key at the given index */
+	return (1);
 }
