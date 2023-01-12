77891d37ba91e73e0cef8f93bb64c75720455256
James Kariuki <karisjaymo99@gmail.com>
2023-01-12 08:18:54 +0300

    new function - hash_node_create

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,32 +20,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL;
 	hash_node_t *collision_node = NULL;
 	unsigned long int k_index;
-	size_t key_size = 0;
-	size_t value_size = 0;
+
+	if (ht == NULL)
+	{
+		return (0);
+	}
 
 	/* Create new hash_node */
-	new_node = malloc(sizeof(hash_node_t));
-
+	new_node = hash_node_create(key, value);
 	if (new_node == NULL)
 	{
-		return (0);
-	}
-
-	/* allocate memory for key*/
-	key_size = strlen(key) + 1;
-	new_node->key = malloc(sizeof(char) * key_size);
-	if (new_node->key == NULL)
-	{
-		/* malloc failure */
-		return (0);
-	}
-
-	/* allocate memory for value*/
-	value_size = strlen(value) + 1;
-	new_node->value = malloc(sizeof(char) * value_size);
-	if (new_node->value == NULL)
-	{
-		/* malloc failure */
 		return (0);
 	}
 
@@ -70,4 +54,49 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	return (1);
+}
+
+/**
+ * hash_node_create - creates a new node for a hash table
+ *
+ * @key: part of the key/value pair of a hash node
+ *			key (cannot be an empty string)
+ *
+ * @value: the value associated with the key (can be an empty str)
+ *
+ * Return: pointer to the new node
+ * OR: NULL on failure.
+ */
+hash_node_t *hash_node_create(const char *key, const char *value)
+{
+	size_t key_size = 0;
+	size_t value_size = 0;
+	hash_node_t *new_node = NULL;
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	/* allocate memory for key*/
+	key_size = strlen(key) + 1;
+	new_node->key = malloc(sizeof(char) * key_size);
+	if (new_node->key == NULL)
+	{
+		/* malloc failure */
+		return (NULL);
+	}
+
+	/* allocate memory for value*/
+	value_size = strlen(value) + 1;
+	new_node->value = malloc(sizeof(char) * value_size);
+	if (new_node->value == NULL)
+	{
+		/* malloc failure */
+		return (NULL);
+	}
+
+	return (new_node);
 }
