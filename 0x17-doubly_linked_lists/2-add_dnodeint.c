b6a7bc2e51d95e017936f2c5a6add71891d39232
James Kariuki <karisjaymo99@gmail.com>
2023-03-16 18:07:23 +0000

    Task 2 - add node to doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -11,13 +12,32 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
+	dlistint_t *first_node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	first_node = *head;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
-		return (NULL)
+		return (NULL);
 	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-
+	if (first_node == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		first_node->prev = new_node;
+		new_node->next = first_node;
+		*head = new_node;
+	}
 	return (new_node);
 }
