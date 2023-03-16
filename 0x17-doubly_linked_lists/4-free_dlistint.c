be96ad4eb2fbe421859553b4b0b1e8c5a6e470ba
James Kariuki <karisjaymo99@gmail.com>
2023-03-16 19:09:21 +0000

    Task 4 - free a dlistint_t list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,9 +8,9 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    if (head != NULL)
-    {
-        free_dlistint(head->next);
-    }
-    free(head);
+	if (head != NULL)
+	{
+		free_dlistint(head->next);
+	}
+	free(head);
 }
