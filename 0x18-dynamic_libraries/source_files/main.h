80410e177fd204f7072c2fd17c46f3c77ea576e6
James Kariuki <karisjaymo99@gmail.com>
2023-03-20 18:43:42 +0000

    Call C functions from python

--- 0x18-dynamic_libraries/source_files/main.h
+++ 0x18-dynamic_libraries/source_files/main.h
@@ -27,7 +27,7 @@ char *_strstr(char *, char *);
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
-/* int div(int a, int b); */
+int div(int a, int b);
 int mod(int a, int b);
 
 #endif /* MAIN_H */
