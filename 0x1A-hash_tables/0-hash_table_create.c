ce157c76e800fef9e9a793f83de3bbed865190cb
James Kariuki <karisjaymo99@gmail.com>
2023-04-06 18:28:54 +0000

    use malloc instead of calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	array = calloc(size, sizeof(hash_node_t *));
+	array = malloc(sizeof(hash_node_t *) * size);
 	ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL || array == NULL)
