6a99e65af96fed8fbff318a417140bd13a6c4829
James Kariuki <karisjaymo99@gmail.com>
2023-04-06 18:43:27 +0000

    test hash_table_create

--- 0x1A-hash_tables/temp.c
+++ 0x1A-hash_tables/temp.c
@@ -13,22 +13,27 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_node_t **array;
+	hash_table_t *ht;
 
-	if (ht != NULL)
+	if (size < 1)
 	{
-		ht->size = size;
-		ht->array = malloc(sizeof(hash_node_t *) * size);
-		if (ht->array == NULL)
-		{
-			return (NULL);
-		}
-
-		for (i = 0; i < size; i++)
-		{
-			ht->array[i] = NULL;
-		}
+		return (NULL);
 	}
 
+	array = calloc(size, sizeof(hash_node_t *));
+	ht = malloc(sizeof(hash_table_t));
+
+	if (ht == NULL || array == NULL)
+	{
+		return (NULL);
+	}
+
+	ht->size = size;
+	ht->array = array;
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}	
 	return (ht);
 }
