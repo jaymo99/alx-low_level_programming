9a1e98af380ee79b9e3afcc58c75fd13dd2219d1
James Kariuki <karisjaymo99@gmail.com>
2023-07-12 12:43:39 +0300

    create search algorithms directory

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * linear_search - find a value in an array using linear search algorithm.
+ * @array: a pointer to the first element of the array to search in.
+ * @size: the number of elements in @array.
+ * @value: the value to look for in @array.
+ * Return: the first index where @value is located.
+ * or -1 if value is not present or array is NULL.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int idx = 0;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", idx, array[idx]);
+		if (value == array[idx])
+		{
+			return (idx);
+		}
+		idx++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,11 @@
+# 0x1E. C - Search Algorithms
+
+## Resources
+* [Search algorithm](https://en.wikipedia.org/wiki/Search_algorithm)
+* [Space complexity (1)](https://www.geeksforgeeks.org/g-fact-86/)
+
+## Learning Objectives
+* What is a search algorithm
+* What is a linear search
+* What is a binary search
+* What is the best search algorithm to use depending on your needs
